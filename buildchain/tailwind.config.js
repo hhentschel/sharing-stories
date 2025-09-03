f19e903552b7c74fc9b56e0e960e72fdddb1b515
Henrik Hentschel <henrik.hentschel@zeix.com>
2025-09-03 16:11:13 +0200

    add new tailwind classes

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -26,10 +26,22 @@ module.exports = {
     },
     container: {
       center: true,
-      padding: '2rem',
+      padding: '0 2rem',
     },
-  },
-  extend: {
+    extend: {
+      colors: {
+        'brand': '#253900',
+        'header': '#08CB00',
+        'nav-link': '#253900',
+        'nav-link-active': '#000000',
+        'custom': {
+            50: '#fef2f2',
+            100: '#fee2e2',
+            500: '#C93127',
+            900: '#7f1d1d',
+          }
+      }
+    },
   },
   plugins: [],
 };
